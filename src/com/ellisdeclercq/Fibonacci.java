6938215ec84812ae2ccb5aefee8142f5b8988dba
Ellis De Clercq <ellisdeclercq@gmail.com>
2018-05-29 15:59:02 +0200

    wiskunde oefeningen toegevoegd

---
+++ src/com/ellisdeclercq/Fibonacci.java
@@ -0,0 +1,40 @@
+package com.ellisdeclercq;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        /*int i = 0;
+        int max = 7;
+
+        while (i <= max) {
+            int power = (int)Math.pow(2, i);
+
+            for(int j = 0; j < i; j++) {
+                System.out.print((int)Math.pow(2, j) + " ");
+            }
+            System.out.println(power);
+
+            for (int k = i - 1; k >= 0; k-- ) {
+                System.out.println((int)Math.pow(2, k) + " ");
+            System.out.println();
+                System.out.println(power);
+            i++;
+        }
+    }
+}
+*/
+
+ int n = 20;
+
+ int a = 0, b = 1;
+ for (int i = 1; i <= n; i++){
+     int som = a + b;
+     a = b;
+     b = som;
+     System.out.println(som);
+
+
+ }
+
+ }
+
+}
